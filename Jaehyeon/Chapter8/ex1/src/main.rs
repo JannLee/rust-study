792191b639b56b19faa919fe0c29b760bc539aa8
이재현 <junog115@gmail.com>
2022-07-01 16:10:53 +0900

    mean 수정
    짝수 -> 가운데 두 수들의 평균
    홀수 -> 가운데 수

--- Jaehyeon/Chapter8/ex1/src/main.rs
+++ Jaehyeon/Chapter8/ex1/src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 use std::collections::HashMap;
 
 fn main() {
-    const NUM_COUNT: u32 = 100;
+    const NUM_COUNT: u32 = 10;
 
     let mut rng = rand::thread_rng();
     let mut numbers: Vec<u32> = Vec::new();
@@ -28,7 +28,19 @@ fn main() {
     println!("\n");
 
     let average = (total as f32) / (numbers.len() as f32);
-    println!("Average: {}, Median: {}, Mode: {}", average, &numbers[((NUM_COUNT / 2) - 1) as usize], get_mode(&numbers));
+    println!("Average: {}, Median: {}, Mode: {}", average, get_mean(&numbers), get_mode(&numbers));
+}
+
+fn get_mean(numbers: &Vec<u32>) -> f64 {
+    if numbers.len() % 2 == 0 { // 짝수면 가운데에 있는 데이터들의 평균 
+        let mut total = *numbers.get(numbers.len() / 2).unwrap();
+        total += *numbers.get(numbers.len() / 2 - 1).unwrap();
+        
+        total as f64 / 2.0
+    }
+    else {
+        *numbers.get(numbers.len() / 2).unwrap() as f64
+    }
 }
 
 fn get_mode(numbers: &Vec<u32>) -> u32 {
