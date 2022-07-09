5867a0f97c8d437f220f0219dcba0b735e38b551
김기덕 <ikid4127@naver.com>
2022-07-09 15:45:36 +0900

    12장 스터디 및 과제 제출
    - 스터디 코드 및 과제 코드 업로드

---
+++ GiDeokKim/chapter12/greprs/src/main.rs
@@ -0,0 +1,21 @@
+extern crate greprs;
+
+use std::env;
+use std::process;
+
+use greprs::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = greprs::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
