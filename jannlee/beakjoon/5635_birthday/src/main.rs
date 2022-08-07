0ae6dbf51d3bcd3e9e74b65b412ec4b635e411ec
이지한 <jannlee@estsecurity.com>
2022-08-07 17:36:50 +0900

    workspace 적용 후 빌드 실패 해결

--- jannlee/beakjoon/5635_birthday/src/main.rs
+++ jannlee/beakjoon/5635_birthday/src/main.rs
@@ -1,16 +1,16 @@
-extern crate beakjoon5635_birthday;
+extern crate birthday;
 
 fn main() {
-    let count = beakjoon5635_birthday::read_number();
+    let count = birthday::read_number();
     let mut i = 0;
     let mut students: Vec<(String, i32, i32, i32)> = Vec::new();
 
     while i < count {
-        students.push(beakjoon5635_birthday::read_student());
+        students.push(birthday::read_student());
         i = i + 1;
     }
 
-    let s = beakjoon5635_birthday::solution(students);
+    let s = birthday::solution(students);
     println!("{}", s.0);
     println!("{}", s.1);
 }
