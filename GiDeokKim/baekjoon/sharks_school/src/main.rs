5423e3029496b1f975e1a4ff9ec3b9ec9ea0df26
김기덕 <ikid4127@naver.com>
2022-08-17 14:27:36 +0900

    [백준 21608번] 상어 초등학교 구현
    - 코드 정리

--- GiDeokKim/baekjoon/sharks_school/src/main.rs
+++ GiDeokKim/baekjoon/sharks_school/src/main.rs
@@ -1,18 +1,15 @@
 use std::io;
 
 fn main() {
-    let grid_n = get_positive_integer();
-    let student_infos = set_student_infos(grid_n);
-    let mut class_room : Vec<Vec<i64>> = vec![vec![0; grid_n]; grid_n];
-    let mut best_positions : Vec<Option<Position>> = Vec::new();
+    let grid_n = get_usize_integer();
+    let student_infos = get_student_infos(grid_n);
+    let mut class_room : Vec<Vec<usize>> = vec![vec![0; grid_n]; grid_n];
+    let mut best_pos : Position;
     let mut result = 0;
 
     for student_info in student_infos.iter() {
-        let mut all_positions = get_best_position(grid_n, student_info, &mut class_room);
-        let best_pos = all_positions.pop();
-        let set_pos = best_pos.clone().unwrap();
-        best_positions.push(best_pos);
-        class_room[(-set_pos.x) as usize][(-set_pos.y) as usize] = student_info.student_number;
+        best_pos = get_best_position(grid_n, student_info, &mut class_room);
+        class_room[(-best_pos.x) as usize][(-best_pos.y) as usize] = student_info.student_number;
     }
     
     for student_info in student_infos.iter() {
@@ -22,56 +19,53 @@ fn main() {
     println!("{}", result);
 }
 
-fn get_positive_integer() -> usize {
+fn get_usize_integer() -> usize {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to get integer");
     input.trim().parse().unwrap()
 }
 
-#[derive(Debug)]
 struct Student {
-    student_number : i64,
-    favorite_friends : [i64; 4]
+    student_number : usize,
+    favorite_friends : [usize; 4]
 }
 
-fn set_student_info() -> Student {
+fn get_student_info() -> Student {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read the student information");
-
-    let information: Vec<i64> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let result = Student {
+    let information: Vec<usize> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
+    
+    Student {
         student_number: information[0],
         favorite_friends: [information[1], information[2], information[3], information[4]],
-    };
-
-    result
+    }
 } 
 
-fn set_student_infos(grid_n : usize) -> Vec<Student> {
+fn get_student_infos(grid_n : usize) -> Vec<Student> {
     let num_students = grid_n * grid_n;
     let mut result = Vec::new();
     let mut student_info : Student;
     
     for _ in 0..num_students {
-        student_info = set_student_info();
+        student_info = get_student_info();
         result.push(student_info);
     }
 
     result
 }
 
-#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Eq, Ord, PartialEq, PartialOrd)]
 struct Position {
-    favorite_friends : i64,
-    empty_seats : i64,
-    x : i64,
-    y : i64,
+    favorite_friends : usize,
+    empty_seats : usize,
+    x : i32,
+    y : i32,
 }
 
-fn get_best_position(grid_n : usize, student_info : &Student, class_room : &mut Vec<Vec<i64>>) -> Vec<Position> {
+fn get_best_position(grid_n : usize, student_info : &Student, class_room : &mut Vec<Vec<usize>>) -> Position {
     let mut positions = Vec::new();
-    let dx : [i64; 4] = [0, 0, 1, -1];
-    let dy : [i64; 4] = [1, -1, 0, 0];
+    let dx : [i32; 4] = [0, 0, 1, -1];
+    let dy : [i32; 4] = [1, -1, 0, 0];
 
     for _x in 0..grid_n {
         for _y in 0..grid_n {
@@ -79,40 +73,40 @@ fn get_best_position(grid_n : usize, student_info : &Student, class_room : &mut 
                 continue;
             }
 
-            let mut _empty_seats : i64 = 0;
-            let mut _favorite_friends : i64 = 0;
+            let mut _empty_seats : usize = 0;
+            let mut _favorite_friends : usize = 0;
             for _index in 0..4 {
-                let coordinate_x = _x as i64 + dx[_index];
-                let coordinate_y = _y as i64 + dy[_index];
+                let coordinate_x = _x as i32 + dx[_index];
+                let coordinate_y = _y as i32 + dy[_index];
                 if coordinate_x < 0 || coordinate_y < 0 || 
-                    coordinate_x >= grid_n as i64 || coordinate_y >= grid_n as i64 {
+                    coordinate_x >= grid_n as i32 || coordinate_y >= grid_n as i32 {
                     continue;
                 }
+
                 if class_room[coordinate_x as usize][coordinate_y as usize] == 0 {
                     _empty_seats += 1;
                 }
                 else {
                     for _friend_index in 0..4 {
-                        let favorite_student = student_info.favorite_friends[_friend_index];
-                        if class_room[coordinate_x as usize][coordinate_y as usize] == favorite_student {
+                        if class_room[coordinate_x as usize][coordinate_y as usize] == student_info.favorite_friends[_friend_index] {
                             _favorite_friends += 1;
                             break;
                         }
                     }
                 }
             }
-            positions.push(Position { x: -(_x as i64), y: -(_y as i64), empty_seats: _empty_seats, favorite_friends: _favorite_friends});
+            positions.push(Position { x: -(_x as i32), y: -(_y as i32), empty_seats: _empty_seats, favorite_friends: _favorite_friends});
         }
     }
     positions.sort();
 
-    positions
+    positions.pop().clone().unwrap()
 }
 
-fn get_score(grid_n : usize, student_info : &Student, class_room : &mut Vec<Vec<i64>>) -> i64 {
+fn get_score(grid_n : usize, student_info : &Student, class_room : &mut Vec<Vec<usize>>) -> i64 {
     let mut score = 0;
-    let dx : [i64; 4] = [0, 0, 1, -1];
-    let dy : [i64; 4] = [1, -1, 0, 0];
+    let dx : [i32; 4] = [0, 0, 1, -1];
+    let dy : [i32; 4] = [1, -1, 0, 0];
 
     for _x in 0..grid_n {
         for _y in 0..grid_n {
@@ -121,21 +115,19 @@ fn get_score(grid_n : usize, student_info : &Student, class_room : &mut Vec<Vec<
             }
 
             for _index in 0..4 {
-                let coordinate_x = _x as i64 + dx[_index];
-                let coordinate_y = _y as i64 + dy[_index];
+                let coordinate_x = _x as i32 + dx[_index];
+                let coordinate_y = _y as i32 + dy[_index];
                 if coordinate_x < 0 || coordinate_y < 0 || 
-                    coordinate_x >= grid_n as i64 || coordinate_y >= grid_n as i64 {
+                    coordinate_x >= grid_n as i32 || coordinate_y >= grid_n as i32 {
                     continue;
                 }
 
                 for _favorite_index in 0..4 {
                     if class_room[coordinate_x as usize][coordinate_y as usize] == student_info.favorite_friends[_favorite_index] {
-                        if score == 0 {
-                            score = 1;
-                        }
-                        else {
-                            score *= 10;
-                        }
+                        match score {
+                            0 => score = 1,
+                            _ => score *= 10,
+                        };
                     }
                 }
             }
