cbcfc1a196421b4ef191de8c4a655559bc516e5c
이재현 <junog115@gmail.com>
2022-09-07 17:08:33 +0900

    잘못 올라간 파일 제거
    파일 제거

--- Jaehyeon/test/add/adder/src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate add_one;
-extern crate rand;
-
-fn main() {
-    let num = 10;
-    println!("Hello, world! {}", add_one::add_one(num));
-}
--- Jaehyeon/test/copy_test/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let v = vec!["abc", "ddd", "eee"];
-
-    let s: String = v[1];
-    println!("{}", s);
-}
